5897c85a6ef528b67b9d310be04efc61d92baad1
Corvette31 <corvette.ss@yandex.ru>
2022-06-15 02:58:08 +0300

    Добавьте файлы проекта.

---
+++ Sequence/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Sequence
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //По условию задачи дано стартовое число в нашем случае 5,
+            //и последовательность чисел, где каждое следующее число
+            //на 7 больше предыдущего, и ограничением последовательности не более 100
+            int stratNumber = 5;
+            int increment = 7;
+            int maxValue = 100;
+
+            for (int i = stratNumber; i < maxValue; i += increment)
+            {
+                Console.Write($"{i} ");
+            }
+        }
+    }
+}
